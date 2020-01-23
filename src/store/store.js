93e089c5670928233858777a5fb1cc07f3fd1209
shafi <frshafi49@gmail.com>
2020-01-23 22:38:10 +0600

    added vuex store

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// store for global state management
+Vue.use(Vuex)
+export const store = new Vuex.Store({
+    // strict: true, // restricts change vuex data without mutation
+
+    state: {
+
+    },
+
+    getters: {
+
+    },
+
+    mutations: {
+
+    }
+})
